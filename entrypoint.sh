c7a4a0b8841e271d844335535e75e19c6a9f88de
RozzenRose <RozzenRose@gmail.com>
2025-08-13 14:34:54 +0200

    db url split

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "Waiting for PostgreSQL at $POSTGRES_HOST:$POSTGRES_PORT..."
-until pg_isready -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" > /dev/null 2>&1; do
+echo "Waiting for PostgreSQL at $DB_HOST:$DB_PORT..."
+until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" > /dev/null 2>&1; do
   sleep 1
 done
 
